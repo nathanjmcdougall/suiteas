6297deb18ae4d035872457c453dcf4e7a8ce9854
Nathan McDougall <nathan.j.mcdougall@gmail.com>
2023-11-05 22:39:18 +1300

    Add `ruff` and comply with it.
    Signed-off-by: Nathan McDougall <nathan.j.mcdougall@gmail.com>

--- src/suiteas/__init__.py
+++ src/suiteas/__init__.py
@@ -1,2 +1,8 @@
-def hello():
+"""The suiteas package."""
+
+def hello() -> None:
+    """Return a greeting.
+
+    This is a placeholder function until the package has content.
+    """
     return "Hello from suiteas!"
