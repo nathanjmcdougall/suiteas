20b4b9758dd4f4b6d7c5edc6c3babe7967428fc3
Nathan McDougall <nathan.j.mcdougall@gmail.com>
2024-01-01 11:37:00 +1300

    Config read-in
    Signed-off-by: Nathan McDougall <nathan.j.mcdougall@gmail.com>

--- src/suiteas/core/read/config.py
+++ src/suiteas/core/read/config.py
@@ -14,110 +14,8 @@ class ConfigFileError(ValueError):
     """An invalid configuration file was found."""
 
 
-def get_config(proj_dir: Path) -> ProjConfig:
-    """Find the configuration for a project in the pyproject.toml file."""
-    toml_path = proj_dir / TOML_NAME
-    toml_config = get_toml_config(toml_path)
-
-    pkg_names = (
-        toml_config.pkg_names
-        if toml_config.pkg_names is not None
-        else toml_config.setuptools_pkg_names
-    )
-    src_rel_path = toml_config.src_rel_path
-    tests_rel_path = toml_config.tests_rel_path
-    unittest_dir_name = toml_config.unittest_dir_name
-
-    if src_rel_path is None:
-        src_rel_path = _heuristic_src_rel_path(proj_dir, pkg_names)
-
-    src_path = proj_dir / src_rel_path
-    if not src_path.exists():
-        msg = f"Could not find source directory {src_path}"
-        raise FileNotFoundError(msg)
-
-    if pkg_names is None:
-        pkg_names = _heuristic_pkg_names(proj_dir, src_rel_path)
-
-    if tests_rel_path is None:
-        tests_rel_path = _heuristic_test_rel_path(proj_dir)
-
-    tests_path = proj_dir / tests_rel_path
-    if not tests_path.exists():
-        msg = f"Could not find tests directory {tests_path}"
-        raise FileNotFoundError(msg)
-
-    if unittest_dir_name is None:
-        unittest_dir_name = _heuristic_unittest_dir_name(tests_path)
-
-    return ProjConfig(
-        pkg_names=pkg_names,
-        src_rel_path=src_rel_path,
-        tests_rel_path=tests_rel_path,
-        unittest_dir_name=unittest_dir_name,
-    )
-
-
-def _heuristic_src_rel_path(proj_dir: Path, pkg_names: list[str] | None) -> Path:
-    if (proj_dir / "src").exists():
-        return Path("src")
-
-    if pkg_names is not None and all(
-        (proj_dir / pkg_name).exists() for pkg_name in pkg_names
-    ):
-        return Path(".")
-    msg = (
-        "Could not automatically determine source directory for project. "
-        "Please manually configure this in pyproject.toml as follows:\n"
-        "[tool.suiteas]\n"
-        'src_rel_path = "???"'
-    )
-    raise ConfigFileError(msg)
-
-
-def _heuristic_pkg_names(proj_dir: Path, src_rel_path: Path) -> list[str]:
-    if src_rel_path != Path("."):
-        return [
-            path.name for path in (proj_dir / src_rel_path).glob("*") if path.is_dir()
-        ]
-
-    msg = (
-        "Could not automatically determine package names for project. "
-        "Please manually configure this in pyproject.toml as follows:\n"
-        "[tool.suiteas]\n"
-        'pkg_names = ["???"]'
-    )
-    raise ConfigFileError(msg)
-
-
-def _heuristic_test_rel_path(proj_dir: Path) -> Path:
-    if (proj_dir / "tests").exists():
-        return Path("tests")
-    if (proj_dir / "test").exists():
-        return Path("test")
-    if (proj_dir / "testing").exists():
-        return Path("testing")
-
-    msg = (
-        "Could not automatically determine tests directory for project. "
-        "Please manually configure this in pyproject.toml as follows:\n"
-        "[tool.suiteas]\n"
-        'tests_rel_path = "???"'
-    )
-    raise ConfigFileError(msg)
-
-
-def _heuristic_unittest_dir_name(tests_path: Path) -> Path:
-    if (tests_path / "unit").exists():
-        return Path("unit")
-
-    msg = (
-        "Could not automatically determine unit tests directory for project. "
-        "Please manually configure this in pyproject.toml as follows:\n"
-        "[tool.suiteas]\n"
-        'unittest_dir_name = "???"'
-    )
-    raise FileNotFoundError(msg)
+class EmptyConfigFileError(ConfigFileError):
+    """An empty configuration file was found."""
 
 
 class TOMLProjConfig(BaseModel):
@@ -127,8 +25,121 @@ class TOMLProjConfig(BaseModel):
     src_rel_path: Path | None = None
     tests_rel_path: Path | None = None
     unittest_dir_name: Path | None = None
+    project_name: str | None = None
     setuptools_pkg_names: list[str] | None = None
     model_config = dict(extra="forbid")
+
+
+def get_config(proj_dir: Path) -> ProjConfig:
+    """Find the configuration for a project in the pyproject.toml file."""
+    toml_path = proj_dir / TOML_NAME
+    try:
+        toml_config = get_toml_config(toml_path)
+    except (FileNotFoundError, EmptyConfigFileError):
+        toml_config = TOMLProjConfig()
+
+    pkg_names = toml_config.pkg_names
+    src_rel_path = toml_config.src_rel_path
+    tests_rel_path = toml_config.tests_rel_path
+    unittest_dir_name = toml_config.unittest_dir_name
+
+    if pkg_names is None:
+        pkg_names = _heuristic1_pkg_names(toml_config=toml_config)
+
+    if src_rel_path is None:
+        src_rel_path = _heuristic_src_rel_path(proj_dir=proj_dir, pkg_names=pkg_names)
+
+    _validate_src_rel_path(src_rel_path, proj_dir=proj_dir)
+
+    if pkg_names is None:
+        pkg_names = _heuristic2_pkg_names()
+
+    if tests_rel_path is None:
+        tests_rel_path = _heuristic_tests_rel_path()
+
+    _validate_tests_rel_path(tests_rel_path, proj_dir=proj_dir)
+
+    if unittest_dir_name is None:
+        unittest_dir_name = _heuristic_unittest_dir_name()
+
+    return ProjConfig(
+        pkg_names=pkg_names,
+        src_rel_path=src_rel_path,
+        tests_rel_path=tests_rel_path,
+        unittest_dir_name=unittest_dir_name,
+    )
+
+
+def _heuristic_src_rel_path(*, proj_dir: Path, pkg_names: list[str] | None) -> Path:
+    if (proj_dir / "src").exists():
+        return Path("src")
+
+    if pkg_names is not None and len(pkg_names) == 1:
+        (pkg_name,) = pkg_names
+        if (proj_dir / pkg_name).exists():
+            return Path(".")
+
+    msg = (
+        "Could not automatically determine source directory for project. "
+        "Please manually configure this in pyproject.toml as follows:\n"
+        "[tool.suiteas]\n"
+        'src_rel_path = "???"'
+    )
+    raise ConfigFileError(msg)
+
+
+def _heuristic1_pkg_names(*, toml_config: TOMLProjConfig) -> list[str] | None:
+    if toml_config.setuptools_pkg_names is not None:
+        return toml_config.setuptools_pkg_names
+
+    if toml_config.project_name is not None:
+        return [toml_config.project_name]
+
+    return None
+
+
+def _heuristic2_pkg_names() -> list[str]:
+    msg = (
+        "Could not automatically determine package names for project. "
+        "Please manually configure this in pyproject.toml as follows:\n"
+        "[tool.suiteas]\n"
+        'pkg_names = ["???"]'
+    )
+    raise ConfigFileError(msg)
+
+
+def _heuristic_tests_rel_path() -> Path:
+    msg = (
+        "Could not automatically determine tests directory for project. "
+        "Please manually configure this in pyproject.toml as follows:\n"
+        "[tool.suiteas]\n"
+        'tests_rel_path = "???"'
+    )
+    raise ConfigFileError(msg)
+
+
+def _heuristic_unittest_dir_name() -> Path:
+    msg = (
+        "Could not automatically determine unit tests directory for project. "
+        "Please manually configure this in pyproject.toml as follows:\n"
+        "[tool.suiteas]\n"
+        'unittest_dir_name = "???"'
+    )
+    raise ConfigFileError(msg)
+
+
+def _validate_src_rel_path(src_rel_path: Path, *, proj_dir: Path) -> None:
+    src_path = proj_dir / src_rel_path
+    if not src_path.exists():
+        msg = f"Could not find source directory {src_path}"
+        raise FileNotFoundError(msg)
+
+
+def _validate_tests_rel_path(tests_rel_path: Path, *, proj_dir: Path) -> None:
+    tests_path = proj_dir / tests_rel_path
+    if not tests_path.exists():
+        msg = f"Could not find tests directory {tests_path}"
+        raise FileNotFoundError(msg)
 
 
 def get_toml_config(toml_path: Path) -> TOMLProjConfig:
@@ -142,7 +153,7 @@ def get_toml_config(toml_path: Path) -> TOMLProjConfig:
 
     if not pyproject_contents:
         msg = f"Configuration file is empty at {toml_path}"
-        raise ConfigFileError(msg)
+        raise EmptyConfigFileError(msg)
 
     parsed_toml: dict[str, Any] = tomllib.loads(pyproject_contents)
     config_by_tool: dict[str, Any] = parsed_toml.get("tool", {})
@@ -158,14 +169,19 @@ def get_toml_config(toml_path: Path) -> TOMLProjConfig:
         raise ConfigFileError(msg)
 
     try:
-        setuptools_config_kwargs = config_by_tool["setuptools"]
-        setuptools_pkg_names = setuptools_config_kwargs["packages"]
+        project_name = parsed_toml["project"]["name"]
+    except KeyError:
+        project_name = None
+
+    try:
+        setuptools_pkg_names = config_by_tool["setuptools"]["packages"]
     except KeyError:
         setuptools_pkg_names = None
 
     try:
         config = TOMLProjConfig(
             **config_kwargs,
+            project_name=project_name,
             setuptools_pkg_names=setuptools_pkg_names,
         )
     except ValidationError as err:
--- tests/unit/suiteas/core/read/test_config.py
+++ tests/unit/suiteas/core/read/test_config.py
@@ -3,7 +3,13 @@ from pathlib import Path
 
 import pytest
 
-from suiteas.core.read.config import ConfigFileError, TOMLProjConfig, get_toml_config
+from suiteas.core.read.config import (
+    ConfigFileError,
+    EmptyConfigFileError,
+    TOMLProjConfig,
+    get_config,
+    get_toml_config,
+)
 
 
 class TestGetTOMLConfig:
@@ -15,12 +21,32 @@ class TestGetTOMLConfig:
             get_toml_config(Path("fakey_mcfake_face.toml"))
 
     def test_empty(self, config_files_parent_dir: Path) -> None:
-        with pytest.raises(ConfigFileError, match="Configuration file is empty at .*"):
+        with pytest.raises(EmptyConfigFileError):
             get_toml_config(config_files_parent_dir / "empty.toml")
+
+    def test_complete(self, config_files_parent_dir: Path) -> None:
+        config = get_toml_config(config_files_parent_dir / "complete.toml")
+
+        assert config == TOMLProjConfig(
+            pkg_names=["foo", "bar"],
+            src_rel_path=Path("mysrc"),
+            tests_rel_path=Path("mytests"),
+            unittest_dir_name=Path("myunit"),
+            setuptools_pkg_names=["foo_other", "bar_other"],
+            project_name="example",
+        )
 
     def test_syntax_error(self, config_files_parent_dir: Path) -> None:
         with pytest.raises(tomllib.TOMLDecodeError):
             get_toml_config(config_files_parent_dir / "syntax_err.toml")
+
+    def test_lists(self, config_files_parent_dir: Path) -> None:
+        """Test where we use singleton lists instead of constants."""
+        with pytest.raises(
+            ConfigFileError,
+            match=".*Input is not a valid path .* input_type=list.*",
+        ):
+            get_toml_config(config_files_parent_dir / "badlists.toml")
 
     def test_no_section(self, config_files_parent_dir: Path) -> None:
         with pytest.raises(
@@ -53,13 +79,37 @@ class TestGetTOMLConfig:
         ):
             get_toml_config(config_files_parent_dir / "extra_subsection.toml")
 
-    def test_complete(self, config_files_parent_dir: Path) -> None:
-        config = get_toml_config(config_files_parent_dir / "complete.toml")
 
-        assert config == TOMLProjConfig(
-            pkg_names=["foo", "bar"],
-            src_rel_path=Path("mysrc"),
-            tests_rel_path=Path("mytests"),
-            unittest_dir_name=Path("myunit"),
-            setuptools_pkg_names=["foo_other", "bar_other"],
-        )
+class TestGetConfig:
+    def test_nonexistent(self, projs_parent_dir: Path) -> None:
+        with pytest.raises(
+            ConfigFileError,
+            match=".*Could not automatically determine source directory.*",
+        ):
+            get_config(projs_parent_dir / "nonexistent")
+
+    def test_empty_config(self, projs_parent_dir: Path) -> None:
+        with pytest.raises(
+            ConfigFileError,
+            match=".*Could not automatically determine source directory.*",
+        ):
+            get_config(projs_parent_dir / "empty_config")
+
+    def test_autosrc(self, projs_parent_dir: Path) -> None:
+        """Test we can automatically find the source folder at src."""
+        get_config(projs_parent_dir / "autosrc")
+
+    def test_autonamesrc(self, projs_parent_dir: Path) -> None:
+        """Test we can determine the source folder is the name of the project."""
+        get_config(projs_parent_dir / "autonamesrc")
+
+    def test_allauto(self, projs_parent_dir: Path) -> None:
+        """Test we can automatically determine everything without pyproject.toml."""
+        raise NotImplementedError
+
+    def test_srcconfig_only(self, projs_parent_dir: Path) -> None:
+        with pytest.raises(
+            ConfigFileError,
+            match=".*Could not automatically determine package names.*",
+        ):
+            get_config(projs_parent_dir / "srcconfig_only")
