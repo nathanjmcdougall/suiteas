bf70a8a7f330691cd9af182a29c7455c554936c1
Nathan McDougall <nathan.j.mcdougall@gmail.com>
2024-01-06 12:41:24 +1300

    Add tests for print_violations
    Signed-off-by: Nathan McDougall <nathan.j.mcdougall@gmail.com>

--- tests/unit/suiteas/core/test_run.py
+++ tests/unit/suiteas/core/test_run.py
@@ -1,9 +1,17 @@
+import io
 import os
+from contextlib import redirect_stderr
 from pathlib import Path
 
 import pytest
 
 from suiteas.core.run import print_violations, run_suiteas, run_suiteas_main
+from suiteas.core.violations import (
+    Violation,
+    empty_pytest_class,
+    missing_test_func,
+    unimported_tested_func,
+)
 
 
 class TestRunSuiteAsMain:
@@ -16,8 +24,86 @@ class TestRunSuiteAsMain:
 
 
 class TestPrintViolations:
-    def test_nothing(self) -> None:
-        _ = print_violations
+    def test_no_violations(self) -> None:
+        f = io.StringIO()
+        with redirect_stderr(f):
+            print_violations([])
+        s = f.getvalue()
+        assert s == ""
+
+    def test_sui001(self) -> None:
+        f = io.StringIO()
+        with redirect_stderr(f):
+            print_violations(
+                [
+                    Violation(
+                        viol_cat=missing_test_func,
+                        rel_path=Path(
+                            "src/fakemcfake/example.py",
+                        ),
+                        line_num=5,
+                        char_offset=4,
+                        fmt_info=dict(
+                            pytest_file_rel_posix=r"tests\fakemcfake\test_example.py",
+                            func="example_func",
+                        ),
+                    ),
+                ],
+            )
+        s = f.getvalue()
+        assert s == (
+            r"src\fakemcfake\example.py:5:4: "
+            r"SUI001 example_func untested in tests\fakemcfake\test_example.py"
+            "\n"
+        )
+
+    def test_sui002(self) -> None:
+        f = io.StringIO()
+        with redirect_stderr(f):
+            print_violations(
+                [
+                    Violation(
+                        viol_cat=empty_pytest_class,
+                        rel_path=Path(
+                            "src/fakemcfake/example.py",
+                        ),
+                        line_num=5,
+                        char_offset=4,
+                        fmt_info=dict(pytest_class_name="TestExample"),
+                    ),
+                ],
+            )
+        s = f.getvalue()
+        assert s == (
+            r"src\fakemcfake\example.py:5:4: SUI002 TestExample has no tests" + "\n"
+        )
+
+    def test_sui003(self) -> None:
+        f = io.StringIO()
+        with redirect_stderr(f):
+            print_violations(
+                [
+                    Violation(
+                        viol_cat=unimported_tested_func,
+                        rel_path=Path(
+                            "src/fakemcfake/example.py",
+                        ),
+                        line_num=0,
+                        char_offset=0,
+                        fmt_info=dict(
+                            pytest_file_rel_posix=r"tests\fakemcfake\test_example.py",
+                            func_fullname="fakemcfake.example.example_func",
+                        ),
+                    ),
+                ],
+            )
+        s = f.getvalue()
+        assert s == (
+            r"src\fakemcfake\example.py:0:0: "
+            r"SUI003 fakemcfake.example.example_func is not imported "
+            r"in tests\fakemcfake\test_example.py"
+            "\n"
+        )
 
 
 class TestRunSuiteAs:
