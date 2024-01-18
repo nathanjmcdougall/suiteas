091d41725f49f5cde52530397b0a8b368eb9300b
Nathan McDougall <nathan.j.mcdougall@gmail.com>
2024-01-18 13:39:34 +1300

    Don't check ignored rules
    Signed-off-by: Nathan McDougall <nathan.j.mcdougall@gmail.com>

--- src/suiteas/core/check.py
+++ src/suiteas/core/check.py
@@ -1,6 +1,8 @@
 """Functionality to check whether a test suite is compliant."""
 
 
+from pathlib import Path
+
 from pydantic.alias_generators import to_snake
 
 from suiteas.core.names import PYTEST_CLASS_PREFIX
@@ -11,7 +13,7 @@ from suiteas.core.violations import (
     missing_test_func,
     unimported_tested_func,
 )
-from suiteas.domain import Func, Project, PytestFile
+from suiteas.domain import File, Func, Project, PytestFile
 
 
 def get_violations(project: Project) -> list[Violation]:
@@ -39,40 +41,67 @@ def get_violations(project: Project) -> list[Violation]:
             if func.is_underscored:
                 continue
 
-            # Check SUI001: missing-test-module
-            if not _pytest_file_has_func_tests(pytest_file=pytest_file, func=func):
-                violations.append(
-                    Violation(
-                        rule=missing_test_func,
-                        rel_path=file.path.relative_to(project.proj_dir),
-                        line_num=func.line_num,
-                        char_offset=func.char_offset,
-                        fmt_info=dict(
-                            func=func.name,
-                            pytest_file_rel_posix=pytest_rel_path.as_posix(),
-                        ),
+            if "SUI001" in project.config.checks:
+                violations.extend(
+                    _get_sui001_violations(
+                        pytest_file=pytest_file,
+                        func=func,
+                        file=file,
+                        project=project,
+                        pytest_rel_path=pytest_rel_path,
                     ),
                 )
 
-            # Check SUI003: unimported-tested-func
-            if not _pytest_file_imports_func(
-                pytest_file=pytest_file,
-                func=func,
-            ):
-                violations.append(
-                    Violation(
-                        rule=unimported_tested_func,
-                        rel_path=file.path.relative_to(project.proj_dir),
-                        line_num=func.line_num,
-                        char_offset=func.char_offset,
-                        fmt_info=dict(
-                            func_fullname=func.full_name,
-                            pytest_file_rel_posix=pytest_rel_path.as_posix(),
-                        ),
+            if "SUI003" in project.config.checks:
+                violations.extend(
+                    _get_sui003_violations(
+                        pytest_file=pytest_file,
+                        func=func,
+                        file=file,
+                        project=project,
+                        pytest_rel_path=pytest_rel_path,
                     ),
                 )
 
     # Check SUI002: empty-pytest-class
+    if "SUI002" in project.config.checks:
+        violations.extend(_get_sui002_violations(project=project))
+
+    return violations
+
+
+def _get_sui001_violations(
+    *,
+    pytest_file: PytestFile | None,
+    func: Func,
+    file: File,
+    project: Project,
+    pytest_rel_path: Path,
+) -> list[Violation]:
+    if not _pytest_file_has_func_tests(
+        pytest_file=pytest_file,
+        func=func,
+    ):
+        return [
+            Violation(
+                rule=missing_test_func,
+                rel_path=file.path.relative_to(project.proj_dir),
+                line_num=func.line_num,
+                char_offset=func.char_offset,
+                fmt_info=dict(
+                    func=func.name,
+                    pytest_file_rel_posix=pytest_rel_path.as_posix(),
+                ),
+            ),
+        ]
+    return []
+
+
+def _get_sui002_violations(
+    *,
+    project: Project,
+) -> list[Violation]:
+    violations = []
     for pytest_file in project.pytest_suite.pytest_files:
         for pytest_class in pytest_file.pytest_classes:
             if not pytest_class.has_funcs:
@@ -83,8 +112,34 @@ def get_violations(project: Project) -> list[Violation]:
                         fmt_info=dict(pytest_class_name=pytest_class.name),
                     ),
                 )
-
     return violations
+
+
+def _get_sui003_violations(
+    *,
+    pytest_file: PytestFile | None,
+    func: Func,
+    file: File,
+    project: Project,
+    pytest_rel_path: Path,
+) -> list[Violation]:
+    if not _pytest_file_imports_func(
+        pytest_file=pytest_file,
+        func=func,
+    ):
+        return [
+            Violation(
+                rule=unimported_tested_func,
+                rel_path=file.path.relative_to(project.proj_dir),
+                line_num=func.line_num,
+                char_offset=func.char_offset,
+                fmt_info=dict(
+                    func_fullname=func.full_name,
+                    pytest_file_rel_posix=pytest_rel_path.as_posix(),
+                ),
+            ),
+        ]
+    return []
 
 
 def _pytest_file_has_func_tests(
