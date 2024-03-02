f2eb6c2956b7eea4a8d59cc4909ec95c4285d192
Nathan McDougall <nathan.j.mcdougall@gmail.com>
2024-03-03 12:50:44 +1300

    Refactor
    Signed-off-by: Nathan McDougall <nathan.j.mcdougall@gmail.com>

--- tests/unit/suiteas/read/test_pytest_collect.py
+++ tests/unit/suiteas/read/test_pytest_collect.py
@@ -4,6 +4,8 @@ import pytest
 
 from suiteas.read.pytest_collect import collect_test_items
 
+TEST_HELLO_PATH = Path("tests/unit/h10mfz3s/test_hello.py")
+
 
 class TestCollectTestItems:
     def test_empty(self, projs_parent_dir: Path) -> None:
@@ -12,7 +14,7 @@ class TestCollectTestItems:
     def test_one_test(self, projs_parent_dir: Path) -> None:
         (item,) = collect_test_items(projs_parent_dir / "one_test")
         (relfspath, lineno, testname) = item.location
-        assert Path(relfspath) == Path("tests/unit/cnmbqr9a/test_hello.py")
+        assert Path(relfspath) == TEST_HELLO_PATH
         assert lineno is not None
         assert lineno + 1 == 19
         assert testname == "TestThings.test_nothing"
@@ -26,26 +28,10 @@ class TestCollectTestItems:
             results.add((Path(relfspath), lineno + 1, testname))
 
         expected = {
-            (
-                Path("tests/unit/h10mfz3s/test_hello.py"),
-                8,
-                "TestThings.test_nothing[True]",
-            ),
-            (
-                Path("tests/unit/h10mfz3s/test_hello.py"),
-                8,
-                "TestThings.test_nothing[False]",
-            ),
-            (
-                Path("tests/unit/h10mfz3s/test_hello.py"),
-                15,
-                "TestParam.test_nothing[True]",
-            ),
-            (
-                Path("tests/unit/h10mfz3s/test_hello.py"),
-                15,
-                "TestParam.test_nothing[False]",
-            ),
+            (TEST_HELLO_PATH, 8, "TestThings.test_nothing[True]"),
+            (TEST_HELLO_PATH, 8, "TestThings.test_nothing[False]"),
+            (TEST_HELLO_PATH, 15, "TestParam.test_nothing[True]"),
+            (TEST_HELLO_PATH, 15, "TestParam.test_nothing[False]"),
         }
 
         assert results == expected
